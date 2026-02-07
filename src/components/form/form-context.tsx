8e0a73b265a356d64e21694a3bd62b3d089cf64c
Domenic Pittari <dom.pittari@gmail.com>
2026-02-07 17:04:22 +0900

    fix: remove lazy field imports

--- src/components/form/form-context.tsx
+++ src/components/form/form-context.tsx
@@ -1,32 +1,11 @@
 import { createFormHook, createFormHookContexts } from "@tanstack/react-form"
-import { lazy } from "react"
-
-const FieldDescription = lazy(() =>
-	import("../field/field-description").then((m) => ({
-		default: m.FieldDescription,
-	})),
-)
-const FieldErrors = lazy(() =>
-	import("../field/field-errors").then((m) => ({
-		default: m.FieldErrors,
-	})),
-)
-const FieldInput = lazy(() =>
-	import("../field/field-input").then((m) => ({
-		default: m.FieldInput,
-	})),
-)
-const FieldLabel = lazy(() =>
-	import("../field/field-label").then((m) => ({
-		default: m.FieldLabel,
-	})),
-)
-const FieldSlider = lazy(() =>
-	import("../field/field-slider").then((m) => ({
-		default: m.FieldSlider,
-	})),
-)
-
+import {
+	FieldDescription,
+	FieldErrors,
+	FieldInput,
+	FieldLabel,
+	FieldSlider,
+} from "../field"
 import { FormErrors } from "./form-errors"
 import { FormReset } from "./form-reset"
 import { FormSubmit } from "./form-submit"
