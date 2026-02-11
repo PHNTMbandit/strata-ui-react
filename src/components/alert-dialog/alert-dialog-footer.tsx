02b79def08653c3f6fbdb2e4e79d1b0429ac1e5f
Domenic Pittari <dom.pittari@gmail.com>
2026-02-11 10:46:06 +0900

    style: change colours

--- src/components/alert-dialog/alert-dialog-footer.tsx
+++ src/components/alert-dialog/alert-dialog-footer.tsx
@@ -9,7 +9,7 @@ export const AlertDialogFooter = ({
 }: AlertDialogFooterProps) => {
 	return (
 		<div
-			className={cn("w-full bg-surface-container-mid", className)}
+			className={cn("w-full bg-surface-container-low", className)}
 			ref={ref}
 			{...props}
 		>
